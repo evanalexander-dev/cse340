7b39061ee2cd61ac7fd058f13761ad2270982c63
Evan Alexander <evan@evanalexander.dev>
2025-03-24 12:32:17 -0600

    W04 Learning Activities

---
+++ routes/accountRoute.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const router = new express.Router() 
+const utilities = require("../utilities/")
+const regValidate = require('../utilities/account-validation')
+const accountController = require("../controllers/accountController")
+
+// Route to build login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+// Route to build the registration view
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+// Route to process the registration form
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,12 +9,39 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+const session = require("express-session")
+const pool = require("./database/")
+const bodyParser = require("body-parser")
 const baseController = require("./controllers/baseController")
 const utilities = require("./utilities/")
 const static = require("./routes/static")
+const accountRoute = require("./routes/accountRoute")
 const inventoryRoute = require("./routes/inventoryRoute")
 const errorRoute = require("./routes/errorRoute")
 
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
@@ -29,6 +56,8 @@ app.set("layout", "./layouts/layout")
 app.use(static)
 // Index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
+// Account routes
+app.use("/account", accountRoute)
 // Inventory routes
 app.use("/inv", inventoryRoute)
 // Error routes
